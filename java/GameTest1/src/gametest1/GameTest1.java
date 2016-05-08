f5572d9a40c338343ee5c440c3051b520b0bb8d2
Ryan Peden <Ryan Peden>
2016-05-07 20:48:39 -0400

    added framerate class

---
+++ java/GameTest1/src/gametest1/GameTest1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gametest1;
+
+/**
+ *
+ * @author ryan0
+ */
+public class GameTest1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hi");
+        try {
+            System.in.read();
+        } catch(Exception e) {}
+        
+    }
+    
+}
